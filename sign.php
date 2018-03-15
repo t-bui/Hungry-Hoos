23027d36009c9706609a3a4dbe5c29c24f568c4d
Tiffany Bui <tvb4sh@virginia.edu>
2018-03-14 20:47:58 -0400

    assignment 4

---
+++ sign.php
@@ -0,0 +1,161 @@
+<!DOCTYPE html>
+<html>
+    
+    <head>
+        <link rel="stylesheet" href="main.css">
+        <title>Hungry Hoos!</title>
+     </head>
+  
+    <body>
+      
+      <header>
+        
+        <h1>Hungry Hoos</h1>
+        <h2>Free Food For Frugal Friends</h2>
+        
+         <ul>
+          <li><a onmouseover = "hover(this)" onmouseout = "away(this)" href="home.html">Home</a></li>
+          <li><a onmouseover = "hover(this)" onmouseout = "away(this)" href="about.html">About Me</a></li>
+          <li><a onmouseover = "hover(this)" onmouseout = "away(this)" href="browse.html">Browse</a></li>
+          <li><a onmouseover = "hover(this)" onmouseout = "away(this)" href="sign.php">Sign In</a></li>
+        </ul>
+        
+        <script>
+          function hover(obj) {
+            obj.style.fontWeight = "bold"
+          }
+          
+          function away(obj) {
+            obj.style.fontWeight = "normal"
+          }
+          
+        </script>
+        
+      </header>
+        
+      <h3>Sign In</h3>
+        
+        
+      <footer>
+      </footer>
+        
+    </body>
+
+    <script type="text/javascript">
+/**
+      function validateForm() {
+        var my_form = document.forms["my_form"];
+
+        //check Name is not empty
+        var x = my_form["Name"].value;
+        if(x == "") {
+          alert("Name field must be filled out!");
+          return false;
+        }
+
+        //Check email is not empty, contains @ and .edu
+        x = my_form["Email"].value
+        if(x == "") {
+          alert("Email field must be filled out!");
+          return false;
+        }
+        if(!validateEmail(x)) {
+          alert("Email must be a valid .edu address!");
+        }
+    
+      }
+**/
+
+      function emptyAlert() {
+      	alert("At least one field was empty. Please fill out all fields!");
+      } 
+
+      function formatAlert() {
+      	alert("Please use a valid email address!");
+      } 
+
+      function bothAlert() {
+      	alert("Please fill out both fields and use a valid email address!");
+      } 	
+  
+/**  
+      function validateEmail(email) {
+        //checks email is of the form:
+        //[chars]@[chars].edu
+        //from http://stackoverflow.com/questions/26702338/setting-a-form-to-only-allow-edu-email-addresses
+       // <!––action="formHandler.php"method="post"––>  goes below in form div
+
+        var regex = /^([\w-]+(?:\.[\w-]+)*)@(virginia+\.)+edu/i;
+        return regex.test(email);
+      }
+**/
+
+    </script>
+    <div id = "main" class="form" style="display:flex;justify-content:center;align-items:center;">
+
+      <form id = "my_form2" action = "<?php $_SERVER['PHP_SELF'] ?>" method = "post">
+        Email:
+        <br />
+        <input type="text" name="Email" class = "inputform" />
+        <br />
+        <br />
+        Password:
+        <br />
+        <input type = "Password" name="pass" class = "inputform" />
+        <br />
+
+        <br />
+        <input type="submit" value="Sign In" />
+        <br />
+        <br />
+        <p style="text-align:center;">New to Hungry Hoos? Sign up here!</p>
+        <a href="signup.php" class="button" style="text-align:center;align-items:center; 75px;">Sign up!</a>
+
+      </form>
+    </div>
+
+ <?php
+
+
+	function sendMessage($e, $eF) {
+
+	  if ($e && !$eF) {
+	    echo '<script>bothAlert()</script>'; 	
+	  } 
+	  else if (!$eF) {
+	    echo '<script>formatAlert()</script>'; 	
+	  }
+          else if ($e) {
+	    echo '<script>emptyAlert()</script>'; 	
+	  } 
+	  else;
+
+
+	}
+
+	
+	if ($_SERVER['REQUEST_METHOD'] == 'POST') {
+ 	
+	 
+	  if (empty($_POST['Email'])) 
+	    $eEmpty = 1;
+ 	  if (empty($_POST['pass'])) 
+	    $pEmpty = 1;
+
+	  if ($eEmpty != NULL || $pEmpty != NULL)
+	    $empty = true;
+ 	  
+        
+	  $e = $_POST['Email'];
+          $emailFormat = filter_var($e, FILTER_VALIDATE_EMAIL);
+	  
+	  sendMessage($empty, $emailFormat);
+          
+
+	}
+
+	
+
+	?>
+
+</html>
